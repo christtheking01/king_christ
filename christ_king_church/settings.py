c5c8157e3e87c2d20337a3880275d84092014c41
spacecadet3008 <emmaleonard3008@gmail.com>
2026-01-17 00:23:53 +0300

    cax

--- christ_king_church/settings.py
+++ christ_king_church/settings.py
@@ -105,6 +105,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'catechesis.context_processors.get_catechesis_context', 
             ],
         },
     },
