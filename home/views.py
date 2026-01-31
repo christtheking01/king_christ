282ac49c0c38c9f333262d6147be84bc403e2609
christtheking01 <christtheking01@yahoo.com>
2026-01-31 19:56:52 +0300

    Update views.py

--- home/views.py
+++ home/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from catechesis.models import SacramentRequest
+from member.models import Member, Community,Committee,Ministry
 
 # Create your views here.
+def home(request):
+    #'upcoming_events': Event.objects.filter(date__gte=timezone.now()).order_by('date')[:3],
+     # Total members in the system
+    member_count = Member.objects.count()
+    
+    # Total number of unique Ministries, Communities, and Committees
+    ministry_count = Ministry.objects.count()
+    community_count = Community.objects.count()
+    committee_count = Committee.objects.count()
+
+    context = {
+        'member_count': member_count,
+        'ministry_count': ministry_count,
+        'community_count': community_count,
+        'committee_count': committee_count,
+        'recent_activities':Member.objects.order_by('id')[:5]
+
+    }
+    return render(request, 'index.html', context)
